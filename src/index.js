70987a7bacb3f12387a8f647ee0b4aa7e6003a0d
Rickard Kamel <rickardkamel@gmail.com>
2016-11-17 21:04:52 +0100

    Removed spacing.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ const App = () => {
     return <div>Hi!</div>;
 }
 
-
 // Take this component's generated HTML and put it on 
 // the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
