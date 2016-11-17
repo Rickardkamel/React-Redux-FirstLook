bb1cbc7e87d24cd39717180c949950f30d9a3c96
Rickard Kamel <rickardkamel@gmail.com>
2016-11-17 21:02:19 +0100

    Inserted component into dom using querySelector.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const App = function() {
     return <div>Hi!</div>;
 }
 
+
 // Take this component's generated HTML and put it on 
 // the page (in the DOM)
-ReactDOM.render(App);
+ReactDOM.render(<App />, document.querySelector('.container'));
