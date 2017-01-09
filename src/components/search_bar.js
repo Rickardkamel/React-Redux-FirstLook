dd2fade8faa2afe77bbfeae64c0d2b0a346587c7
rickardkamel <rickardkamel@gmail.com>
2017-01-09 16:16:17 +0100

    Working with states.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {term: ''};
+    }
+
     render() {
-        return <input />;
+        return (
+            <div>
+                <input onChange={event => this.setState({term: event.target.value})}/>
+                <br/>
+                Value of the input: {this.state.term}
+            </div>
+        );
     }
 }
 
 export default SearchBar;
-
-// testing a git commit.
